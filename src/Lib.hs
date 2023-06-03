7c140494bfc2bfa3d9e3c7ba39b0c6f68cc06536
markcornwell <mark.cornwell@me.com>
2023-06-02 20:13:39 -0400

    ChatGPT generated program to bounce object

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,12 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import System.Exit (exitSuccess)
 
+data GameMode = SquareMode | CircleMode deriving Eq
+
 data GameState = GameState
     { circlePos :: Point      -- Position of the circle
     , circleVel :: Vector     -- Velocity of the circle
+    , gameMode :: GameMode    -- Current game mode (SquareMode or CircleMode)
     }
 
 windowWidth :: Int
@@ -31,23 +34,29 @@ initialState :: GameState
 initialState = GameState
     { circlePos = (0, 0)
     , circleVel = (100, 100)
+    , gameMode = SquareMode  -- Start with SquareMode
     }
 
 drawGame :: GameState -> IO Picture
-drawGame gameState = do
-    return (translateX (circlePos gameState))
+drawGame gameState
+  | gameMode gameState == SquareMode = return (translateX (circlePos gameState)) -- Draw square in SquareMode
+  | gameMode gameState == CircleMode = return (renderCircle (circlePos gameState)) -- Draw circle in CircleMode
+
 
 handleEvent :: Event -> GameState -> IO GameState
 handleEvent (EventKey (SpecialKey KeyEsc) Down _ _) _ = exitSuccess
+handleEvent (EventKey (Char 'c') Down _ _) gameState = return gameState { gameMode = CircleMode } -- Switch to CircleMode
+handleEvent (EventKey (Char 's') Down _ _) gameState = return gameState { gameMode = SquareMode } -- Switch to SquareMode
 handleEvent _ gameState = return gameState
 
 updateGame :: Float -> GameState -> IO GameState
 updateGame deltaTime gameState = do
     let circlePos'@(x, y) = moveCircle (circleVel gameState) deltaTime (circlePos gameState)
         (minX, minY) = (-fromIntegral windowWidth / 2 + circleRadius, -fromIntegral windowHeight / 2 + circleRadius)
-        (maxX, maxY) = (fromIntegral windowWidth / 2 - circleRadius, fromIntegral windowHeight / 2 - circleRadius)
-        circleVel' = if x < minX || x > maxX then (-fst (circleVel gameState), snd (circleVel gameState)) else (fst (circleVel gameState), snd (circleVel gameState))
-    return gameState { circlePos = clampPoint circlePos' (minX, minY) (maxX, maxY), circleVel = circleVel' }
+        (maxX, maxY) = ( fromIntegral windowWidth / 2 - circleRadius,  fromIntegral windowHeight / 2 - circleRadius)
+        circleVel'@(vx, vy) = if x < minX || x > maxX then (-fst (circleVel gameState), snd (circleVel gameState)) else circleVel gameState
+        circleVel'' = if y < minY || y > maxY then (fst circleVel', -snd circleVel') else circleVel'
+    return gameState { circlePos = clampPoint circlePos' (minX, minY) (maxX, maxY), circleVel = circleVel'' }
 
 moveCircle :: Vector -> Float -> Point -> Point
 moveCircle (vx, vy) dt (x, y) = (x + vx * dt, y + vy * dt)
@@ -63,4 +72,12 @@ clamp val lowerBound upperBound
     | otherwise = val
 
 translateX :: Point -> Picture
-translateX (x, y) = translate x y (color red (rectangleSolid 100 50))
+translateX (x, y) = translate x y (color red (rectangleSolid 100 100))
+
+render :: GameState -> IO Picture
+render gameState
+    | gameMode gameState == SquareMode = return (translateX (circlePos gameState)) -- Draw square in SquareMode
+    | gameMode gameState == CircleMode = return (renderCircle (circlePos gameState)) -- Draw circle in CircleMode
+
+renderCircle :: Point -> Picture
+renderCircle (x, y) = translate x y $ color red $ circleSolid circleRadius
