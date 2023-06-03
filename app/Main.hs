51eeb51abb796b0a8001ac60ddf990c8a95bd68d
markcornwell <mark.cornwell@me.com>
2023-06-03 13:50:59 -0400

    got readerT working wo gloss playIO

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
-module Main(main) where
+module Main (main) where
 
-import MyLib
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Reader (ReaderT, ask, runReaderT)
+import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
 
+data AppState = AppState
+  { counter :: IORef Int
+  }
+
+type App = ReaderT AppState IO
+
+-- Helper function to increment the counter
+incrementCounter :: App ()
+incrementCounter = do
+  state <- ask
+  liftIO $ modifyIORef (counter state) (+ 1)
+
+-- Example render function
+render :: App ()
+render = do
+  state <- ask
+  count <- liftIO $ readIORef (counter state)
+  liftIO $ putStrLn $ "Counter: " ++ show count
+
+-- Example state transformation function
+stateTransformation :: App ()
+stateTransformation = do
+  incrementCounter
+  incrementCounter
+
+-- Example main function
 main :: IO ()
-main = someFunction
+main = do
+  counterRef <- newIORef 0
+  let initialState = AppState { counter = counterRef }
+  runReaderT (render >> stateTransformation >> render) initialState
