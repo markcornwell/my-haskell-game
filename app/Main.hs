1ee1ce5ef790c4ebd6c60ab48acf98052bd55ab9
markcornwell <mark.cornwell@me.com>
2023-06-07 12:20:25 -0400

    dead end

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,16 @@
 module Main (main) where
 
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Reader (ReaderT, ask, runReaderT)
+import Control.Monad.Trans.Reader (ReaderT, ask, runReaderT)
 import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
+import Graphics.Gloss.Interface.IO.Game -- (Picture, playIO)
 
 data AppState = AppState
   { counter :: IORef Int
   }
 
 type App = ReaderT AppState IO
+
 
 -- Helper function to increment the counter
 incrementCounter :: App ()
@@ -17,15 +19,17 @@ incrementCounter = do
   liftIO $ modifyIORef (counter state) (+ 1)
 
 -- Example render function
-render :: App ()
+render :: App Picture
 render = do
   state <- ask
   count <- liftIO $ readIORef (counter state)
   liftIO $ putStrLn $ "Counter: " ++ show count
+  -- Return a placeholder picture for demonstration
+  pure $ error "Placeholder picture"
 
--- Example state transformation function:q
-stateTransformation :: App ()
-stateTransformation = do
+-- Example state transformation function
+stateTransformation :: Float -> App AppState
+stateTransformation _dt = do
   incrementCounter
   incrementCounter
 
@@ -34,4 +38,11 @@ main :: IO ()
 main = do
   counterRef <- newIORef 0
   let initialState = AppState { counter = counterRef }
-  runReaderT (render >> stateTransformation >> render) initialState
+  playIO
+    (InWindow "My Game" (800, 600) (200, 200)) -- Display configuration
+    white                                      -- Background color
+    60                                         -- Frames per second
+    initialState                               -- Initial state
+    (runReaderT render)                        -- Render function
+    (\dt state -> runReaderT (stateTransformation dt) state)
+ -- State transformation function
